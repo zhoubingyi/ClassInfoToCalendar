c21d7b1bab53d6734c5c72af401e9a81bf1584c1
12 <hyeogjoo@gmail.com>
2018-04-22 17:29:47 +0800

    修改
    名称里不显示节数，地点，地点写入到日历系统的 LOCATION 中

--- main.py
+++ main.py
@@ -28,26 +28,6 @@ def main():
 	classInfoHandle();
 	icsCreateAndSave();
 
-def classICSCreate(classInfo):
-	global classTimeList, DONE_ALARMUID, DONE_UnitUID
-	i = int(classInfo["classTime"]-1)
-	className = classInfo["className"]+"|"+classTimeList[i]["name"]+"|"+classInfo["classroom"]
-	endTime = classTimeList[i]["endTime"]
-	startTime = classTimeList[i]["startTime"]
-	for date in classInfo["date"]:
-		eventString = "BEGIN:VEVENT\nCREATED:"+classInfo["CREATED"]
-		eventString = eventString+"\nUID:"+classInfo["UID"]
-		eventString = eventString+"\nDTEND;TZID=Asia/Shanghai:"+date+"T"+endTime
-		eventString = eventString+"00\nTRANSP:OPAQUE\nX-APPLE-TRAVEL-ADVISORY-BEHAVIOR:AUTOMATIC\nSUMMARY:"+className
-		eventString = eventString+"\nDTSTART;TZID=Asia/Shanghai:"+date+"T"+startTime+"00"
-		eventString = eventString+"\nDTSTAMP:"+DONE_CreatedTime
-		eventString = eventString+"\nSEQUENCE:0\nBEGIN:VALARM\nX-WR-ALARMUID:"+DONE_ALARMUID
-		eventString = eventString+"\nUID:"+DONE_UnitUID
-		eventString = eventString+"\nTRIGGER:"+DONE_reminder
-		eventString = eventString+"\nDESCRIPTION:事件提醒\nACTION:DISPLAY\nEND:VALARM\nEND:VEVENT\n"
-		return eventString
-	print("classICSCreate")		
-	
 def save(string):
      f = open("class.ics", 'wb')
      f.write(string.encode("utf-8"))
@@ -59,9 +39,11 @@ def icsCreateAndSave():
 	eventString = ""
 	for classInfo in classInfoList :
 		i = int(classInfo["classTime"]-1)
-		className = classInfo["className"]+"|"+classTimeList[i]["name"]+"|"+classInfo["classroom"]
+		className = classInfo["className"]
 		endTime = classTimeList[i]["endTime"]
 		startTime = classTimeList[i]["startTime"]
+		LOCATION = classInfo["classroom"]
+
 		index = 0
 		for date in classInfo["date"]:
 			eventString = eventString+"BEGIN:VEVENT\nCREATED:"+classInfo["CREATED"]
@@ -73,9 +55,12 @@ def icsCreateAndSave():
 			eventString = eventString+"\nSEQUENCE:0\nBEGIN:VALARM\nX-WR-ALARMUID:"+DONE_ALARMUID
 			eventString = eventString+"\nUID:"+DONE_UnitUID
 			eventString = eventString+"\nTRIGGER:"+DONE_reminder
-			eventString = eventString+"\nDESCRIPTION:事件提醒\nACTION:DISPLAY\nEND:VALARM\nEND:VEVENT\n"
-
+			eventString = eventString+"\nDESCRIPTION:事件提醒\nACTION:DISPLAY"
+			eventString = eventString+"\nEND:VALARM"
+			eventString = eventString+"\nLOCATION:"+LOCATION
+			eventString = eventString+"\nEND:VEVENT\n"
 			index += 1
+
 	icsString = icsString + eventString + "END:VCALENDAR"
 	save(icsString)
 	print("icsCreateAndSave")
@@ -90,14 +75,14 @@ def classInfoHandle():
 		startWeek = json.dumps(classInfo["week"]["startWeek"])
 		endWeek = json.dumps(classInfo["week"]["endWeek"])
 		weekday = float(json.dumps(classInfo["weekday"]))
-		
+
 		dateLength = float((int(startWeek) - 1) * 7)
 		startDate = datetime.datetime.fromtimestamp(int(time.mktime(DONE_firstWeekDate))) + datetime.timedelta(days = dateLength + weekday - 1)
 		string = startDate.strftime('%Y%m%d')
 
 		dateLength = float((int(endWeek) - 1) * 7)
 		endDate = datetime.datetime.fromtimestamp(int(time.mktime(DONE_firstWeekDate))) + datetime.timedelta(days = dateLength + weekday - 1)
-		
+
 		date = startDate
 		dateList = []
 		dateList.append(string)
@@ -137,10 +122,10 @@ def CreateTime():
 	print("CreateTime")
 
 def uniteSetting():
-	# 
+	#
 	global DONE_ALARMUID
 	DONE_ALARMUID = random_str(30) + "&Chanjh.com"
-	# 
+	#
 	global DONE_UnitUID
 	DONE_UnitUID = random_str(20) + "&Chanjh.com"
 	print("uniteSetting")
@@ -152,7 +137,7 @@ def setClassTime():
 	global classTimeList
 	classTimeList = data["classTime"]
 	print("setclassTime")
-	
+
 def setClassInfo():
 	data = []
 	with open('conf_classInfo.json', 'r') as f:
@@ -168,7 +153,7 @@ def setFirstWeekDate(firstWeekDate):
 
 def setReminder(reminder):
 	global DONE_reminder
-	reminderList = ["-PT10M","-PT20M","-PT1H","-PT2H","-P1D"]
+	reminderList = ["-PT20M","-PT30M","-PT1H","-PT2H","-P1D"]
 	if(reminder == "1"):
 		DONE_reminder = reminderList[0]
 	elif(reminder == "2"):
@@ -196,7 +181,7 @@ def checkFirstWeekDate(firstWeekDate):
 	# 长度判断
 	if(len(firstWeekDate) != 8):
 		return NO;
-	
+
 	year = firstWeekDate[0:4]
 	month = firstWeekDate[4:6]
 	date = firstWeekDate[6:8]
@@ -219,7 +204,7 @@ def basicSetting():
 	info = "请设置第一周的星期一日期(如：20160905):\n"
 	firstWeekDate = raw_input(info)
 	checkInput(checkFirstWeekDate, firstWeekDate)
-	
+
 	info = "正在配置上课时间信息……\n"
 	print(info)
 	try :
@@ -236,7 +221,7 @@ def basicSetting():
 	except :
 		sys_exit()
 
-	info = "正在配置提醒功能，请输入数字选择提醒时间\n【0】不提醒\n【1】上课前 10 分钟提醒\n【2】上课前 20 分钟提醒\n【3】上课前 1 小时提醒\n【4】上课前 2 小时提醒\n【5】上课前 1 天提醒\n"
+	info = "正在配置提醒功能，请输入数字选择提醒时间\n【0】不提醒\n【1】上课前 20 分钟提醒\n【2】上课前 30 分钟提醒\n【3】上课前 1 小时提醒\n【4】上课前 2 小时提醒\n【5】上课前 1 天提醒\n"
 	reminder = raw_input(info)
 	checkInput(checkReminder, reminder)
 
@@ -250,7 +235,7 @@ def checkInput(checkType, input):
 			setFirstWeekDate(input)
 	elif(checkType == checkReminder):
 		if(checkReminder(input)):
-			info = "输入有误，请重新输入\n【1】上课前 10 分钟提醒\n【2】上课前 20 分钟提醒\n【3】上课前 1 小时提醒\n【4】上课前 2 小时提醒\n【5】上课前 1 天提醒\n"
+			info = "输入有误，请重新输入\n【1】上课前 20 分钟提醒\n【2】上课前 30 分钟提醒\n【3】上课前 1 小时提醒\n【4】上课前 2 小时提醒\n【5】上课前 1 天提醒\n"
 			reminder = raw_input(info)
 			checkInput(checkReminder, reminder)
 		else:
